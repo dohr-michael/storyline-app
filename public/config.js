966977d38e3df3ea2a425ecc7d21a54c91b59b6b
Michael DOHR <dohr.michael@gmail.com>
2019-04-28 11:34:18 +0200

    config

--- public/config.js
+++ public/config.js
@@ -1,13 +1,3 @@
-/*
-auth0: {
-    domain: 'dohrm.eu.auth0.com',
-    redirectUri: `${window.location.origin}/login`,
-    clientID: 'JszrUbr453U4WMnXDduNqn9yGdGVDKF2',
-    responseType: 'token id_token',
-    scope: 'openid profile email',
-}
- */
-
 window.Config = {
     GRAPHQL_URL: 'http://localhost:3000',
     AUTH0_DOMAIN: 'dohrm.eu.auth0.com',
